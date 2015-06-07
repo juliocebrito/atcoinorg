5f28bc5906dc4a726e86faf3446b8ebad58cf3d1
Julio Brito <jucebridu@gmail.com>
2015-06-06 22:18:41 -0500

    master

--- atcoinorg/settings.py
+++ atcoinorg/settings.py
@@ -62,9 +62,40 @@ WSGI_APPLICATION = 'atcoinorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+class AuthRouter(object):
+
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'auth':
+            return 'users'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'auth':
+            return 'users'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'auth' or \
+           obj2._meta.app_label == 'auth':
+           return True
+        return None
+
+    def allow_migrate(self, db, app_label, model=None, **hints):
+        if app_label == 'auth':
+            return db == 'users'
+        return None
+
+DATABASE_ROUTERS = ['atcoinorg.settings.AuthRouter']
+
 DATABASES = {
     'default': {
         'ENGINE': 'djangae.db.backends.appengine'
+    },
+    'users': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '/cloudsql/atcoinorg:users',
+        'NAME': 'mysql',
+        'USER': 'root'
     }
 }
 
