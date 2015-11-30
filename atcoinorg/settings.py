03f849e3bc1b7eb9c46bf38ea766943387ba69fa
Julio Brito <jucebridu@gmail.com>
2015-11-30 17:07:12 -0500

    development

--- atcoinorg/settings.py
+++ atcoinorg/settings.py
@@ -77,9 +77,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_FILE_STORAGE = 'djangae.storage.BlobstoreStorage'
-
 FILE_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024
-
 FILE_UPLOAD_HANDLERS = (
     'djangae.storage.BlobstoreFileUploadHandler',
     'django.core.files.uploadhandler.MemoryFileUploadHandler',
